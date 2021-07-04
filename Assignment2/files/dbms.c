3943cd4ba9cebbf9f5bb666e16447e6ecc11407e
Royan Andree Saril <50606471+andreeSaRiL@users.noreply.github.com>
2021-07-05 11:07:46 +1200

    Assignment 2

---
+++ Assignment2/files/dbms.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "dbms.h"
+
+
+// Task 6
+int db_show_row(struct db_table *db, unsigned int row)
+{
+}
+
+// Task 7
+int db_add_row(struct db_table *db, struct album *a)
+{  
+}
+
+// Task 8
+int db_remove_row(struct db_table *db, unsigned long id)
+{  
+}
+
+
+
